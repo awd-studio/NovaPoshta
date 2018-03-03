d59fa6806a842b12475e402fee516c7488de8af9
Anton Karpov <awd.com.ua@gmail.com>
2018-03-03 15:28:16 +0200

    Address: Add all Tests

--- tests/NP/Model/AddressTest.php
+++ tests/NP/Model/AddressTest.php
@@ -46,4 +46,94 @@ class AddressTest extends TestCase
         $this->assertInstanceOf(Model::class, $this->instance->searchSettlementsAction());
     }
 
+
+    /**
+     * @covers \NP\Model\Address::getCitiesAction
+     */
+    public function testGetCitiesAction()
+    {
+        $this->assertInstanceOf(Model::class, $this->instance->getCitiesAction());
+    }
+
+
+    /**
+     * @covers \NP\Model\Address::saveAction
+     */
+    public function testSaveAction()
+    {
+        $this->assertInstanceOf(Model::class, $this->instance->saveAction());
+    }
+
+
+    /**
+     * @covers \NP\Model\Address::updateAction
+     */
+    public function testUpdateAction()
+    {
+        $this->assertInstanceOf(Model::class, $this->instance->updateAction());
+    }
+
+
+    /**
+     * @covers \NP\Model\Address::deleteAction
+     */
+    public function testDeleteAction()
+    {
+        $this->assertInstanceOf(Model::class, $this->instance->deleteAction());
+    }
+
+
+    /**
+     * @covers \NP\Model\Address::getSettlementsAction
+     */
+    public function testGetSettlementsAction()
+    {
+        $this->assertInstanceOf(Model::class, $this->instance->getSettlementsAction());
+    }
+
+
+    /**
+     * @covers \NP\Model\Address::getAreasAction
+     */
+    public function testGetAreasAction()
+    {
+        $this->assertInstanceOf(Model::class, $this->instance->getAreasAction());
+    }
+
+
+    /**
+     * @covers \NP\Model\Address::getWarehousesAction
+     */
+    public function testGetWarehousesAction()
+    {
+        $this->assertInstanceOf(Model::class, $this->instance->getWarehousesAction());
+    }
+
+
+    /**
+     * @covers \NP\Model\Address::getWarehouseTypesAction
+     */
+    public function testGetWarehouseTypesAction()
+    {
+        $this->assertInstanceOf(Model::class, $this->instance->getWarehouseTypesAction());
+    }
+
+
+    /**
+     * @covers \NP\Model\Address::getStreetAction
+     */
+    public function testGetStreetAction()
+    {
+        $this->assertInstanceOf(Model::class, $this->instance->getStreetAction());
+    }
+
+
+    /**
+     * @covers \NP\Model\Address::searchSettlementStreetsAction
+     */
+    public function testSearchSettlementStreetsAction()
+    {
+        $this->assertInstanceOf(Model::class, $this->instance->searchSettlementStreetsAction());
+    }
+
 }
